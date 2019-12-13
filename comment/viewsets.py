39884cf846db43a936b5e171069bc48c8d11692b
corentin <corentinalcoy@gmail.com>
2019-12-13 12:24:57 +0100

    implement attribut permission

--- comment/viewsets.py
+++ comment/viewsets.py
@@ -1,11 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from comment.models import Comment
 from comment.serializers import CommentSerializer
+from event.models import Event
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
     def get_queryset(self):
-        return Comment.objects.filter(event_id=self.kwargs['event_pk'])
+        event_pk = self.kwargs['event_pk']
+        event = get_object_or_404(Event.objects.filter(id=event_pk), **{'id': event_pk})
+        return Comment.objects.filter(event=event)
--- user/serializers.py
+++ user/serializers.py
@@ -6,6 +6,23 @@ from user.models import User
 class UserSerializer(serializers.ModelSerializer):
     created = serializers.DateTimeField(source='date_joined', read_only=True)
 
+    def create(self, validated_data):
+        """
+        Without override this method the password is not encrypted
+        :param validated_data: dict
+        :return: user: User
+        """
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    def to_representation(self, instance):
+        user = super(UserSerializer, self).to_representation(instance)
+        if not self.context['request'].user.is_superuser and not self.context['request'].user == instance:
+            user.pop('email')
+            user.pop('is_active')
+            user.pop('username')
+        return user
+
     class Meta:
         model = User
         fields = (
@@ -19,12 +36,3 @@ class UserSerializer(serializers.ModelSerializer):
             'id': {'read_only': True, },
             'password': {'write_only': True, },
         }
-
-    def create(self, validated_data):
-        """
-        Without override this method the password is not encrypted
-        :param validated_data: dict
-        :return: user: User
-        """
-        user = User.objects.create_user(**validated_data)
-        return user
